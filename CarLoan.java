c6bb3bb62e49151ec325f4f8d4fef5d5d48434f2
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-20 18:37:34 +0000

    Initial commit.

---
+++ CarLoan.java
@@ -0,0 +1,16 @@
+public class CarLoan {
+    public static void main(String[] args) {
+
+        int carLoan = 10000;
+        int loanLength = 3;
+        int interestRate = 5;
+        int downPayment = 2000;
+
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("rror! You must take out a valid car loan.");
+        } else if (downPayment >= carLoan) {
+
+        }
+
+    }
+}
