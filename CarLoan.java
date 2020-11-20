a0f2c38bfd6433e9edcdaadba6cba6ebbe21554b
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-20 19:01:14 +0000

    Create monthly loan calculator.

--- CarLoan.java
+++ CarLoan.java
@@ -1,4 +1,5 @@
-public class CarLoan {
+public class CarLoan { // Calculate the monthly car payment a user should expect to make after taking
+                       // out a car loan.
     public static void main(String[] args) {
 
         int carLoan = 10000;
@@ -7,9 +8,16 @@ public class CarLoan {
         int downPayment = 2000;
 
         if (loanLength <= 0 || interestRate <= 0) {
-            System.out.println("rror! You must take out a valid car loan.");
+            System.out.println("Error! You must take out a valid car loan.");
         } else if (downPayment >= carLoan) {
-
+            System.out.println("The car can be paid in full.");
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months; // monthly payment excluding interest
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.print(monthlyPayment);
         }
 
     }
